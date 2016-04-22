fda5252457587128e7356106f5fb7d6a64f2f120
Benjamin Spain <benjamin@thespains.net>
2016-04-22 16:37:42 -0500

    Interative ListItem creation using ng-repeat and ng-show to prevent empty elements.

--- PHPSiteWithAngularJS/Scripts/app.js
+++ PHPSiteWithAngularJS/Scripts/app.js
@@ -4,9 +4,20 @@
 
     app.controller('CountryController', function () {
 
-        this.countries = {
-            name: 'United States'
-        }
+        this.countries = [{
+            name: 'United States',
+            code: 'en',
+            states: [{ name: 'Minnesota'}, {name: 'Iowa'}]
+        },
+        {
+            name: 'Germany',
+            code: 'de',
+            states: [{ name: 'Bavaria' }, { name: 'Berlin' }]
+        },
+        {
+            name: 'Luxemberg',
+            code: 'lu'
+        }]
 
     });
 })();
