759a0e9b8535d39467e928f9ef91e12233b2b939
Benjamin Spain <benjamin@thespains.net>
2016-04-22 19:59:12 -0500

    App.js consumes JSON from getCountries service.

--- PHPSiteWithAngularJS/Scripts/app.js
+++ PHPSiteWithAngularJS/Scripts/app.js
@@ -2,22 +2,31 @@
     
     var app = angular.module('funwithcountries', []);
 
-    app.controller('CountryController', function () {
+    app.controller('CountryController', function ($http) {
 
-        this.countries = [{
-            name: 'United States',
-            code: 'us',
-            states: [{ name: 'Minnesota'}, {name: 'Iowa'}]
-        },
-        {
-            name: 'Germany',
-            code: 'de',
-            states: [{ name: 'Bavaria' }, { name: 'Berlin' }]
-        },
-        {
-            name: 'Luxemberg',
-            code: 'lu'
-        }]
+        //this.countries = [{
+        //    name: 'United States',
+        //    code: 'us',
+        //    states: [{ name: 'Minnesota'}, {name: 'Iowa'}]
+        //},
+        //{
+        //    name: 'Germany',
+        //    code: 'de',
+        //    states: [{ name: 'Bavaria' }, { name: 'Berlin' }]
+        //},
+        //{
+        //    name: 'Luxemberg',
+        //    code: 'lu'
+        //}]
+
+        var that = this;
+
+        $http({
+            method: 'GET',
+            url: 'services/getCountries.php'
+        }).success(function (data) {
+            that.countries = data;
+        });
 
     });
 })();
